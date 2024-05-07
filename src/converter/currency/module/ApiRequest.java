d769b25f7bbf7e8c3fd69ceee431a0c7e215bcf4
Daniel Queiroz <126373330+usuariodaniel@users.noreply.github.com>
2024-05-07 13:35:22 -0300

    Update ApiRequest.java

--- src/converter/currency/module/ApiRequest.java
+++ src/converter/currency/module/ApiRequest.java
@@ -1,6 +1,29 @@
-package moeda.conversor.modulos;
+package converter.currency.module;
 
-public class apiRequest {
+import com.google.gson.Gson;
 
-    public Conversor
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiRequest {
+
+    public ConverterData currencyConverter(String firstCurrency, String secondCurrency){
+        URI adress = URI.create("https://v6.exchangerate-api.com/v6/63ab09d19512ed54e4e39a49/pair/" + firstCurrency + "/" + secondCurrency);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(adress)
+                .build();
+
+        try {
+            HttpResponse<String> response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), ConverterData.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Erro ao obter os dados do API");
+        }
+    }
 }
